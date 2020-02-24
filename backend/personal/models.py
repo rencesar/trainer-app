969b158151c8480ac5c93cd8f9ce34fdf7434913
Renato César <rencesarlira@gmail.com>
2020-02-24 10:18:14 -0300

    Adding users type Personal and Athlete

--- backend/personal/models.py
+++ backend/personal/models.py
@@ -2,11 +2,9 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.translation import gettext as _
 
-from utils.models import OurUser
 
-UserModel = get_user_model()
+class Personal(models.Model):
 
-
-class Personal(OurUser, models.Model):
-    user = models.OneToOneField(UserModel, on_delete=models.CASCADE, related_name='personal', null=False)
+    def __str__(self):
+        return _('Personal: %s' % self.user.first_name)
 
